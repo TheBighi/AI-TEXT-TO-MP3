4d5e42691491a4ccfa74cab8532b8c26cef7f16a
TheBighi <162053604+TheBighi@users.noreply.github.com>
2024-08-22 21:23:14 +0300

    Create groqmp3.py

---
+++ groqmp3.py
@@ -0,0 +1,35 @@
+from gtts import gTTS
+import subprocess
+import os
+from groq import Groq
+
+apikey = "API_KEY_HERE"
+
+question = input("What would you like to ask GROQ: ")
+
+client = Groq(
+    api_key=apikey,
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": question,
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+response = chat_completion.choices[0].message.content
+
+tts = gTTS(response)
+
+file_path = (f'YOUR DESIRED FILE PATH')
+tts.save(file_path)
+
+print(f"Audio saved as file.mp3")
+
+os.startfile(file_path)
+
+print(f"\033[96m{response}\033[0m") #i made it cyan cause cool
